df7246ab7725a99e7d292c97c1038e75a85e0949
Ezra Peisach <ezra.peisach@rcsb.org>
2019-04-19 09:04:01 -0400

    Test import of NEFTranslator

---
+++ wwpdb/utils/tests-nmr/NEFImportTests.py
@@ -0,0 +1,22 @@
+##
+# File: DetachImportTests.py
+# Date:  06-Oct-2018  E. Peisach
+#
+# Updates:
+##
+"""Test cases for DetachUtils - simply import everything to ensure imports work"""
+
+import unittest
+
+from wwpdb.utils.nmr.NEFTranslator.NEFTranslator import NEFTranslator
+
+class ImportTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def testInstantiate(self):
+        c = NEFTranslator()
+
+
+if __name__ == '__main__':
+    unittest.main()

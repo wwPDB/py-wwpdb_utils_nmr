cee4e460751ce08b66ea60446e4b2c8299fc83c3
Ezra Peisach <ezra.peisach@rcsb.org>
2020-06-07 11:10:44 -0400

    2.5.dev2 ensure pynmrstar version is 2.X for python 2

--- setup.py
+++ setup.py
@@ -43,8 +43,12 @@ setup(
     #
     # munkres 1.0.12 last to support python 2. Developers did not tag properly
     install_requires=['wwpdb.utils.config ~= 0.9', 'wwpdb.utils.align',
-                      'pynmrstar >= 2.6.5', 'pytz', "munkres==1.0.12; python_version == '2.7'", 
-                      "munkres; python_version >= '3'", 'mmcif', 'numpy',
+                      "pynmrstar ~= 2.6; python_version < '3'",
+                      "pynmrstar; python_version >= '3'",                      
+                      'pytz',
+                      "munkres==1.0.12; python_version == '2.7'", 
+                      "munkres; python_version >= '3'",
+                      'mmcif', 'numpy',
                       "packaging"],
     packages=find_packages(exclude=['wwpdb.utils.tests-nmr', 'mock-data']),
     # Enables Manifest to be used

b1f30c09524c8abdc2315080d07ef801a4639e2d
Ezra Peisach <ezra.peisach@rcsb.org>
2021-06-17 07:21:53 -0400

    0.11.dev2

--- wwpdb/utils/nmr/__init__.py
+++ wwpdb/utils/nmr/__init__.py
@@ -1,6 +1,6 @@
-
+kjioooou 
 __docformat__ = "restructuredtext en"
 __author__ = "Ezra Peisach"
 __email__ = "ezra.peisach@rcsb.org"
 __license__ = "Apache 2.0"
-__version__ = "0.11.dev1"
+__version__ = "0.11.dev2"

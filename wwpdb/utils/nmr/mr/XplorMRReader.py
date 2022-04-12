442cad7d07a8a129b8d965c9170f6eeda40e4d7e
yokochi47 <yokochi47@gmail.com>
2022-04-12 18:39:08 +0900

    Bug fix in XPLOR-NIH/CNS lexer grammar to avoid confusion between wildcard and line comment

--- wwpdb/utils/nmr/mr/XplorMRReader.py
+++ wwpdb/utils/nmr/mr/XplorMRReader.py
@@ -169,5 +169,10 @@ if __name__ == "__main__":
 
     reader = XplorMRReader(True)
     reader.setDebugMode(True)
-    reader.parse('../../tests-nmr/mock-data-pdbstat/D_1000243168_mr-upload_P2.xplor-nih.V1',  # atom_sel_expr_example.txt',
+    reader.parse('../../tests-nmr/mock-data-pdbstat/D_1000243168_mr-upload_P2.xplor-nih.V1',
+                 '../../tests-nmr/mock-data-pdbstat/6pvr.cif')
+
+    reader = XplorMRReader(True)
+    reader.setDebugMode(True)
+    reader.parse('../../tests-nmr/mock-data-pdbstat/atom_sel_expr_example.txt',
                  '../../tests-nmr/mock-data-pdbstat/6pvr.cif')

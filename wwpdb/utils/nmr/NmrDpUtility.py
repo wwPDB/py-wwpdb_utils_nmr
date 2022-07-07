6e4b5f3bf73ce7da47210ef1fea5080c89f5d523
yokochi47 <yokochi47@gmail.com>
2022-07-07 11:24:00 +0900

    Ignore missing of _Atom_chem_shift.Ambiguity_set_ID tag during remediation (1q80)

--- wwpdb/utils/nmr/NmrDpUtility.py
+++ wwpdb/utils/nmr/NmrDpUtility.py
@@ -18672,13 +18672,25 @@ class NmrDpUtility:
                             err = chk_row_tmp % (chain_id, seq_id, comp_id, atom_id)\
                                 + f"] {ambig_code_name} {str(ambig_code)!r} requires {ambig_set_id_name} loop tag."
 
-                            self.report.error.appendDescription('missing_mandatory_item',
-                                                                {'file_name': file_name, 'sf_framecode': sf_framecode, 'category': lp_category,
-                                                                 'description': err})
-                            self.report.setError()
+                            if self.__remediation_mode:
 
-                            if self.__verbose:
-                                self.__lfh.write(f"+NmrDpUtility.__validateCSValue() ++ LookupError  - {err}\n")
+                                self.report.warning.appendDescription('missing_data',
+                                                                      {'file_name': file_name, 'sf_framecode': sf_framecode, 'category': lp_category,
+                                                                       'description': err})
+                                self.report.setWarning()
+
+                                if self.__verbose:
+                                    self.__lfh.write(f"+NmrDpUtility.__validateCSValue() ++ Warning  - {err}\n")
+
+                            else:
+
+                                self.report.error.appendDescription('missing_mandatory_item',
+                                                                    {'file_name': file_name, 'sf_framecode': sf_framecode, 'category': lp_category,
+                                                                     'description': err})
+                                self.report.setError()
+
+                                if self.__verbose:
+                                    self.__lfh.write(f"+NmrDpUtility.__validateCSValue() ++ LookupError  - {err}\n")
 
                         else:
 
@@ -18712,7 +18724,7 @@ class NmrDpUtility:
                                     self.report.setError()
 
                                     if self.__verbose:
-                                        self.__lfh.write(f"+NmrDpUtility.__validateCSValue() ++ LookupError  - {err}\n")
+                                        self.__lfh.write(f"+NmrDpUtility.__validateCSValue() ++ ValueError  - {err}\n")
 
                                 # Intra-residue ambiguities
                                 elif ambig_code == 4:

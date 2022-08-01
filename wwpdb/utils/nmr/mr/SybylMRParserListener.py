a6c57f17031f292604957d8b3bc5875097b89824
yokochi47 <yokochi47@gmail.com>
2022-08-01 18:35:41 +0900

    Fix StopIteration exception when atom_not_found errors can not be resolved (2m6i)

--- wwpdb/utils/nmr/mr/SybylMRParserListener.py
+++ wwpdb/utils/nmr/mr/SybylMRParserListener.py
@@ -239,7 +239,7 @@ class SybylMRParserListener(ParseTreeListener):
 
                 trimSequenceAlignment(self.__seqAlign, self.__chainAssign)
 
-                if 'Atom not found' in self.warningMessage:
+                if 'Atom not found' in self.warningMessage and self.__reasons is None:
 
                     seqIdRemap = []
 

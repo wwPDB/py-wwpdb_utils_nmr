ef2d8f87ae3a3dcf3b577ecab9ae4c3af3819988
yokochi47 <yokochi47@gmail.com>
2022-09-29 19:31:19 +0900

    Drop debug code

--- wwpdb/utils/nmr/mr/DynamoMRReader.py
+++ wwpdb/utils/nmr/mr/DynamoMRReader.py
@@ -194,9 +194,9 @@ class DynamoMRReader:
 if __name__ == "__main__":
     reader = DynamoMRReader(True)
     reader.setDebugMode(True)
-    reader.parse('../../tests-nmr/mock-data-remediation/1qvx/1qvx-trimmed-div_ext.mr',
+    reader.parse('../../tests-nmr/mock-data-remediation/1qvx/1qvx-trimmed-div_dst-div_src.mr',
                  '../../tests-nmr/mock-data-remediation/1qvx/1qvx.cif')
-    """
+
     reader = DynamoMRReader(True)
     reader.setDebugMode(True)
     reader.parse('../../tests-nmr/mock-data-remediation/2ju5/2ju5-trimmed-div_dst.mr',
@@ -211,4 +211,3 @@ if __name__ == "__main__":
     reader.setDebugMode(True)
     reader.parse('../../tests-nmr/mock-data-remediation/7kn0/dObsA_ngel2.tab',
                  '../../tests-nmr/mock-data-remediation/7kn0/7kn0.cif')
-    """

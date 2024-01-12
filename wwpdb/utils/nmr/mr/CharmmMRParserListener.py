75edb82f08cdd0ad4819d9aeed3d40de41509db9
yokochi47 <yokochi47@gmail.com>
2024-01-12 17:24:52 +0900

    Merge commits from develop branch

--- wwpdb/utils/nmr/mr/CharmmMRParserListener.py
+++ wwpdb/utils/nmr/mr/CharmmMRParserListener.py
@@ -2692,7 +2692,7 @@ class CharmmMRParserListener(ParseTreeListener):
                             if not foundCompId:
                                 # DAOTHER-9063
                                 ligands = 0
-                                if self.__hasNonPolySeq and self.__cur_subtype == 'dist':
+                                if self.__hasNonPoly and self.__cur_subtype == 'dist':
                                     for np in self.__nonPoly:
                                         ligands += len(np['seq_id'])
                                 if ligands == 1 and len(_factor['chain_id']) == 1 and len(_factor['seq_id']) == 1:

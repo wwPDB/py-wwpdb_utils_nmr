1af5af56db02c76a0eb3a0a4499e13eb8f5d785b
yokochi47 <yokochi47@gmail.com>
2024-08-07 15:30:22 +0900

    Reconciled fix for 2miz and 2n3r

--- wwpdb/utils/nmr/NEFTranslator/NEFTranslator.py
+++ wwpdb/utils/nmr/NEFTranslator/NEFTranslator.py
@@ -5670,7 +5670,7 @@ class NEFTranslator:
                     return (atom_list, ambiguity_code, details)
 
             if atom_id.startswith('Q') or atom_id.startswith('M'):
-                min_len = 4 if atom_id.startswith('QQ') else 3 if atom_id.startswith('M') else 1  # ILE/LEU/THR:QB -> HB
+                min_len = 4 if atom_id.startswith('QQ') else 1  # ILE/LEU/THR:QB -> HB (2m6i), MG -> Magnesium (2n3r)
                 if atom_id[-1].isalnum():
                     atom_list, ambiguity_code, details = self.get_star_atom_for_ligand_remap(comp_id, 'H' + atom_id[1:] + '%', details, coord_atom_site, methyl_only)
                     if details is not None and comp_id not in monDict3 and self.__csStat.peptideLike(comp_id) and len(atom_id) > 1\
@@ -6250,7 +6250,7 @@ class NEFTranslator:
                         return (atom_list, ambiguity_code, details)
 
                 if atom_id.startswith('Q') or atom_id.startswith('M'):
-                    min_len = 4 if atom_id.startswith('QQ') else 3 if atom_id.startswith('M') else 1  # ILE/LEU/THR:QB -> HB
+                    min_len = 4 if atom_id.startswith('QQ') else 1  # ILE/LEU/THR:QB -> HB (2m6i), MG -> Magnesium (2n3r)
                     if atom_id[-1].isalnum():
                         atom_list, ambiguity_code, details = self.get_star_atom(comp_id, 'H' + atom_id[1:] + '%', details, leave_unmatched, methyl_only)
                         if details is not None and comp_id not in monDict3 and self.__csStat.peptideLike(comp_id) and len(atom_id) > 1\

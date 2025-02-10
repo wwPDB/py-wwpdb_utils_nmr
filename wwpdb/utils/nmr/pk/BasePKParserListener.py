125a34def43579d18447fb91e4c06c3d404eeb16
yokochi47 <yokochi47@gmail.com>
2025-02-10 15:40:49 +0900

    DAOTHER-8905: Adjust estimation of C-alipharic region

--- wwpdb/utils/nmr/pk/BasePKParserListener.py
+++ wwpdb/utils/nmr/pk/BasePKParserListener.py
@@ -871,7 +871,7 @@ class BasePKParserListener():
                                         __v['atom_type'] = 'C'
                                         __v['atom_isotope_number'] = 13
                                         __v['axis_code'] = 'C'  # all
-                                    elif 30 < center < 50:
+                                    elif 30 < center < 60:
                                         __v['atom_type'] = 'C'
                                         __v['atom_isotope_number'] = 13
                                         __v['axis_code'] = 'C-aliphatic'
@@ -909,7 +909,7 @@ class BasePKParserListener():
                                     __v['spectral_region'] = 'H-methyl'
                                 elif 60 < center < 90 and atom_type == 'C':
                                     __v['spectral_region'] = 'C'  # all
-                                elif 30 < center < 50 and atom_type == 'C':
+                                elif 30 < center < 60 and atom_type == 'C':
                                     __v['spectral_region'] = 'C-aliphatic'
                                 elif 10 < center < 20 and atom_type == 'C':
                                     __v['spectral_region'] = 'C-methyl'

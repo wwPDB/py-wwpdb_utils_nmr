06f58973c55a9004be365cd62209fbbfff1ff06c
yokochi47 <yokochi47@gmail.com>
2025-05-19 13:36:59 +0900

    DAOTHER-7829: Reconciled fix for 2lpe and 2l5y

--- wwpdb/utils/nmr/mr/CharmmMRParserListener.py
+++ wwpdb/utils/nmr/mr/CharmmMRParserListener.py
@@ -1102,15 +1102,16 @@ class CharmmMRParserListener(ParseTreeListener):
                         del self.reasonsForReParsing['global_sequence_offset']
                     no_gap = True
                     for ps in self.__polySeq:
-                        if ps['auth_chain_id'] not in self.reasonsForReParsing['global_auth_sequence_offset']:
+                        chainId = ps['auth_chain_id']
+                        if chainId not in self.reasonsForReParsing['global_auth_sequence_offset']:
                             if 'gap_in_auth_seq' in ps and ps['gap_in_auth_seq']:
                                 offset = next(seq_id - auth_seq_id for seq_id, auth_seq_id in zip(ps['seq_id'], ps['auth_seq_id']))
                                 if any(abs(seq_id - auth_seq_id - offset) > 20 for seq_id, auth_seq_id in zip(ps['seq_id'], ps['auth_seq_id'])):
-                                    failed_ps = next((failed_ps for failed_ps in self.__polySeqRstFailed if failed_ps['chain_id'] == ps['auth_chain_id']), None)
+                                    failed_ps = next((failed_ps for failed_ps in self.__polySeqRstFailed if failed_ps['chain_id'] == chainId), None)
                                     if failed_ps is None:
                                         continue
                                     if any(seq_id in ps['seq_id'] and seq_id not in ps['auth_seq_id'] for seq_id in failed_ps['seq_id']):
-                                        seqIdRemapForRemaining.append({'chain_id': ps['auth_chain_id'], 'seq_id_dict': dict(zip(ps['seq_id'], ps['auth_seq_id']))})
+                                        seqIdRemapForRemaining.append({'chain_id': chainId, 'seq_id_dict': dict(zip(ps['seq_id'], ps['auth_seq_id']))})
                             elif any(seq_id in ps['seq_id'] and seq_id not in ps['auth_seq_id'] for seq_id in ps['seq_id']):
                                 safe = True
                                 for _ps in self.__polySeq:
@@ -1119,12 +1120,14 @@ class CharmmMRParserListener(ParseTreeListener):
                                             safe = False
                                             break
                                 if safe:
-                                    seqIdRemapForRemaining.append({'chain_id': ps['auth_chain_id'], 'seq_id_dict': dict(zip(ps['seq_id'], ps['auth_seq_id']))})
+                                    seqIdRemapForRemaining.append({'chain_id': chainId, 'seq_id_dict': dict(zip(ps['seq_id'], ps['auth_seq_id']))})
                             elif 'gap_in_auth_seq' in ps and ps['gap_in_auth_seq']:
-                                del self.reasonsForReParsing['global_auth_sequence_offset'][ps['auth_chain_id']]
-                                if len(self.reasonsForReParsing['global_auth_sequence_offset']) == 0:
-                                    del self.reasonsForReParsing['global_auth_sequence_offset']
-                                no_gap = False
+                                _ps = next((_ps for _ps in self.__polySeqRstFailed if _ps['chain_id'] == chainId), None)
+                                if _ps is None or not all(_seq_id in ps['seq_id'] for _seq_id, _comp_id in zip(_ps['seq_id'], _ps['comp_id']) if _comp_id not in emptyValue):
+                                    del self.reasonsForReParsing['global_auth_sequence_offset'][chainId]
+                                    if len(self.reasonsForReParsing['global_auth_sequence_offset']) == 0:
+                                        del self.reasonsForReParsing['global_auth_sequence_offset']
+                                    no_gap = False
                     if no_gap:
                         del self.reasonsForReParsing['label_seq_scheme']
                 if 'inhibit_label_seq_scheme' in self.reasonsForReParsing:
@@ -1138,15 +1141,16 @@ class CharmmMRParserListener(ParseTreeListener):
                 if 'label_seq_scheme' in self.reasonsForReParsing:
                     no_gap = True
                     for ps in self.__polySeq:
-                        if ps['auth_chain_id'] not in self.reasonsForReParsing['global_auth_sequence_offset']:
+                        chainId = ps['auth_chain_id']
+                        if chainId not in self.reasonsForReParsing['global_auth_sequence_offset']:
                             if 'gap_in_auth_seq' in ps and ps['gap_in_auth_seq']:
                                 offset = next(seq_id - auth_seq_id for seq_id, auth_seq_id in zip(ps['seq_id'], ps['auth_seq_id']))
                                 if any(abs(seq_id - auth_seq_id - offset) > 20 for seq_id, auth_seq_id in zip(ps['seq_id'], ps['auth_seq_id'])):
-                                    failed_ps = next((failed_ps for failed_ps in self.__polySeqRstFailed if failed_ps['chain_id'] == ps['auth_chain_id']), None)
+                                    failed_ps = next((failed_ps for failed_ps in self.__polySeqRstFailed if failed_ps['chain_id'] == chainId), None)
                                     if failed_ps is None:
                                         continue
                                     if any(seq_id in ps['seq_id'] and seq_id not in ps['auth_seq_id'] for seq_id in failed_ps['seq_id']):
-                                        seqIdRemapForRemaining.append({'chain_id': ps['auth_chain_id'], 'seq_id_dict': dict(zip(ps['seq_id'], ps['auth_seq_id']))})
+                                        seqIdRemapForRemaining.append({'chain_id': chainId, 'seq_id_dict': dict(zip(ps['seq_id'], ps['auth_seq_id']))})
                             elif any(seq_id in ps['seq_id'] and seq_id not in ps['auth_seq_id'] for seq_id in ps['seq_id']):
                                 safe = True
                                 for _ps in self.__polySeq:
@@ -1155,12 +1159,14 @@ class CharmmMRParserListener(ParseTreeListener):
                                             safe = False
                                             break
                                 if safe:
-                                    seqIdRemapForRemaining.append({'chain_id': ps['auth_chain_id'], 'seq_id_dict': dict(zip(ps['seq_id'], ps['auth_seq_id']))})
+                                    seqIdRemapForRemaining.append({'chain_id': chainId, 'seq_id_dict': dict(zip(ps['seq_id'], ps['auth_seq_id']))})
                         elif 'gap_in_auth_seq' in ps and ps['gap_in_auth_seq']:
-                            del self.reasonsForReParsing['global_auth_sequence_offset'][ps['auth_chain_id']]
-                            if len(self.reasonsForReParsing['global_auth_sequence_offset']) == 0:
-                                del self.reasonsForReParsing['global_auth_sequence_offset']
-                            no_gap = False
+                            _ps = next((_ps for _ps in self.__polySeqRstFailed if _ps['chain_id'] == chainId), None)
+                            if _ps is None or not all(_seq_id in ps['seq_id'] for _seq_id, _comp_id in zip(_ps['seq_id'], _ps['comp_id']) if _comp_id not in emptyValue):
+                                del self.reasonsForReParsing['global_auth_sequence_offset'][chainId]
+                                if len(self.reasonsForReParsing['global_auth_sequence_offset']) == 0:
+                                    del self.reasonsForReParsing['global_auth_sequence_offset']
+                                no_gap = False
                     if no_gap:
                         del self.reasonsForReParsing['label_seq_scheme']
                 if 'label_seq_offset' in self.reasonsForReParsing:

97bfc862eae057535be099bb489ad6273c9a4877
yokochi47 <yokochi47@gmail.com>
2025-07-04 20:26:19 +0900

    DAOTHER-7829: Enable to predict sequence offset purely from __seqAtmRstFailed (2l12 unit test)

--- wwpdb/utils/nmr/mr/CnsMRReader.py
+++ wwpdb/utils/nmr/mr/CnsMRReader.py
@@ -227,6 +227,17 @@ if __name__ == "__main__":
     reader = CnsMRReader(False)
     reader.setDebugMode(False)
     reader_listener, _, _ =\
+        reader.parse('../../tests-nmr/mock-data-remediation/2l12/2l12-trimmed.mr',
+                     '../../tests-nmr/mock-data-remediation/2l12/2l12.cif')
+    print(reader_listener.getReasonsForReparsing())
+    reader = CnsMRReader(True, reasons=reader_listener.getReasonsForReparsing())
+    reader.setDebugMode(True)
+    reader.parse('../../tests-nmr/mock-data-remediation/2l12/2l12-trimmed.mr',
+                 '../../tests-nmr/mock-data-remediation/2l12/2l12.cif')
+
+    reader = CnsMRReader(False)
+    reader.setDebugMode(False)
+    reader_listener, _, _ =\
         reader.parse('../../tests-nmr/mock-data-remediation/2knh/2knh-trimmed.mr',
                      '../../tests-nmr/mock-data-remediation/2knh/2knh.cif')
     print(reader_listener.getReasonsForReparsing())

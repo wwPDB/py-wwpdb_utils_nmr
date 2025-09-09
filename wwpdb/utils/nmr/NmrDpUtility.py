b575dfb40bfe2cec17da237ecc0560fcbe90c8f0
yokochi47 <yokochi47@gmail.com>
2025-09-10 08:39:09 +0900

    DAOTHER-7902, 8905, 9785: Reconciled fix for D_1300043061 and D_1300009464

--- wwpdb/utils/nmr/NmrDpUtility.py
+++ wwpdb/utils/nmr/NmrDpUtility.py
@@ -27159,6 +27159,7 @@ class NmrDpUtility:
                     _seq_key = seq_key if seq_key in coord_atom_site else _seq_key
                 if _seq_key in coord_atom_site\
                    and (coord_atom_site[_seq_key]['comp_id'] == comp_id
+                        or (_seq_key not in coord_unobs_res and coord_atom_site[_seq_key]['comp_id'] not in monDict3)
                         or (_seq_key in coord_unobs_res and coord_unobs_res[_seq_key]['comp_id'] != comp_id)):  # 8b9r: A:24:VAL (unobserved), A:24:CU
                     _coord_atom_site = coord_atom_site[_seq_key]
                     _atom_site_atom_id = _coord_atom_site['atom_id']

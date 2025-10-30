fa6e6737e363389c168317b0cdf9012ea5ff6364
yokochi47 <yokochi47@gmail.com>
2025-10-30 16:53:58 +0900

    DAOTHER-9785: Add 'LPS' and 'Lipopolysaccharide' as 'bacterial outer membrane'

--- wwpdb/utils/nmr/ann/BMRBAnnTasks.py
+++ wwpdb/utils/nmr/ann/BMRBAnnTasks.py
@@ -1651,6 +1651,8 @@ class BMRBAnnTasks:
                                     lp.data[idx][type_col] = 'G-quadruplex stabilizing agent'
                                 elif 'bicelle' in mol_common_name or 'phage' in mol_common_name:
                                     lp.data[idx][type_col] = 'molecular alignment inductor'
+                                elif 'lps' in mol_common_name or 'lipopolysaccharide' in mol_common_name:
+                                    lp.data[idx][type_col] = 'bacterial outer membrane'
                                 elif 'phosph' in mol_common_name\
                                     and ('ylethanolamine' in mol_common_name or 'ylcholine' in mol_common_name
                                          or 'ylserine' in mol_common_name or 'ylinositol' in mol_common_name

7d9c8fa1ff9a54d519c596987316caab9a237236
yokochi47 <yokochi47@gmail.com>
2026-02-10 09:41:04 +0900

    Do not sort entry_interview saveframe tags due to existance of undefined tags in the schema file: xlschem_ann.csv

--- wwpdb/utils/nmr/CifToNmrStar.py
+++ wwpdb/utils/nmr/CifToNmrStar.py
@@ -903,7 +903,8 @@ class CifToNmrStar:
             strData.frame_list.sort(key=sf_key)
 
             for sf in strData.frame_list:
-                sf.sort_tags(self.pynmrstar_schema)
+                if sf.category != 'entry_interview':
+                    sf.sort_tags(self.pynmrstar_schema)
                 if len(sf.loops) > 1:
                     sf.loops.sort(key=lp_key)
                 # Iterate through the loops

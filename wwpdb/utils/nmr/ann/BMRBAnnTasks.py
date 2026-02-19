2bbe0a123f54d441d3694fca754548d8fb3b2905
yokochi47 <yokochi47@gmail.com>
2026-02-19 16:28:10 +0900

    Add SDS as micelles

--- wwpdb/utils/nmr/ann/BMRBAnnTasks.py
+++ wwpdb/utils/nmr/ann/BMRBAnnTasks.py
@@ -1716,7 +1716,7 @@ class BMRBAnnTasks:
                                     lp.data[idx][type_col] = 'G-quadruplex stabilizing agent'
                                 elif 'bicelle' in mol_common_name or 'phage' in mol_common_name or mol_common_name == 'pf1':
                                     lp.data[idx][type_col] = 'molecular alignment inductor'
-                                elif mol_common_name in ('dpc', 'dpc-d38') or 'dodecylphosphocholine' in mol_common_name\
+                                elif mol_common_name in ('dpc', 'dpc-d38', 'sds') or 'dodecylphosphocholine' in mol_common_name\
                                         or 'micelle' in mol_common_name:
                                     lp.data[idx][type_col] = 'micelles'
                                 elif mol_common_name in ('chaps', 'chapso') or mol_common_name.startswith('zwittergent'):

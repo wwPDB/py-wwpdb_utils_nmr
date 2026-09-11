78bf11b0fdd55dec6b4231b343ed7d97d897cb06
yokochi47 <yokochi47@gmail.com>
2026-09-11 09:24:57 +0900

    Fix potential exceptions

--- wwpdb/utils/nmr/NmrDpValidationOutStats.py
+++ wwpdb/utils/nmr/NmrDpValidationOutStats.py
@@ -358,8 +358,6 @@ class NmrDpValidationOutStats(NmrDpValidationBase):
             except KeyError:
                 pass
 
-            has_coordinate = self._reg.cifChecked
-
             cif_poly_seq = None
 
             src_id = self._reg.report.getInputSourceIdOfCoord()
@@ -389,7 +387,7 @@ class NmrDpValidationOutStats(NmrDpValidationBase):
 
             # model
 
-            if has_coordinate:
+            if self._reg.cifChecked:
                 model_info = {'file_name': os.path.basename(self._reg.cifPath),
                               'file_type': 'pdbx',
                               'file_size': os.path.getsize(self._reg.cifPath),
@@ -456,7 +454,7 @@ class NmrDpValidationOutStats(NmrDpValidationBase):
                 completeness = vrpt_cs['completeness']
 
                 cs_summary = {}
-                if has_coordinate and completeness['well_defined'][1] > 0:
+                if self._reg.cifChecked and completeness['well_defined'][1] > 0:
                     cs_summary['number_of_target_shifts_in_well_defined_region'] =\
                         completeness['well_defined'][1]
                     cs_summary['number_of_assigned_shifts_in_well_defined_region'] =\
@@ -1290,7 +1288,7 @@ class NmrDpValidationOutStats(NmrDpValidationBase):
 
                         sf_info['number_of_parsed'] = len(consist_ids)
 
-                        if has_coordinate:
+                        if self._reg.cifChecked:
 
                             if content_subtype == 'chem_shift':
 
